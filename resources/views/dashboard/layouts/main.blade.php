0325b62901d8c877949fe864c85070302b155559
annddri <andriwidani@gmail.com>
2025-02-05 11:47:23 +0700

    create post form

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -21,6 +21,16 @@
     <link href="/css/dashboard.css" rel="stylesheet">
     <!-- feather icons -->
     <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+
+    <!-- trix editor -->
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
+    <script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>
+
+    <style>
+      trix-toolbar [data-trix-button-group="file-tools"]{
+        display: none;
+      }
+    </style>
   </head>
   <body>
 
